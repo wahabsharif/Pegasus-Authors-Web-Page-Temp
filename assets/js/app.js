a15e6c6760e15ab59d26b4338923506a1468816a
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-03-05 15:38:59 +0500

    First Commint

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".menu-list a");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop - 100;
+            if (pageYOffset >= sectionTop) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").includes(current)) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
+
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    const hamburger = document.querySelector(".hamburger");
+
+    if (menu.classList.contains("active")) {
+        menu.style.height = "0";
+        menu.style.padding = "0";
+    } else {
+        menu.style.height = "100vh";
+        menu.style.padding = "20px 0";
+    }
+
+    menu.classList.toggle("active");
+    hamburger.textContent = menu.classList.contains("active") ? "✖" : "☰";
+}
